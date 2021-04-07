098da1b8475a057eea177a551d37ada2605360ba
plat67 <good2060@naver.com>
2021-04-07 19:33:44 +0900

    edit gitignore

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,4 @@
- public abstract class Main {
+public abstract class Main {
 
     private static Runningback[] nflplayersreference;
 
@@ -6,33 +6,33 @@
 
     private static WideReceiver[] nflplayers;
 
-    public static void main(String args[]){
+    public static void main(String args[]) {
 
-    Runningback r = new Runningback("Thomlinsion");
+        Runningback r = new Runningback("Thomlinsion");
 
-    Quarterback q = new Quarterback("Tom Brady");
+        Quarterback q = new Quarterback("Tom Brady");
 
-    WideReceiver w = new WideReceiver("Steve Smith");
+        WideReceiver w = new WideReceiver("Steve Smith");
 
-    NFLPlayersReference[] NFLPlayersReference;
+        NFLPlayersReference[] NFLPlayersReference;
 
 
         Run();// {
 
-        NFLPlayersReference = new NFLPlayersReference [3];
+        NFLPlayersReference = new NFLPlayersReference[3];
 
         nflplayersreference[0] = r;
 
         players[1] = q;
 
         nflplayers[2] = w;
- 
 
-            for ( int i = 0; i < nflplayersreference.length; i++ ) {
 
-            System.out.println("My name is " + " nflplayersreference[i].getName());
+        for (int i = 0; i < nflplayersreference.length; i++) {
 
-            nflplayersreference[i].run();
+            System.out.println("My name is " + nflplayersreference[i].getName());
+
+                    nflplayersreference[i].run();
 
             nflplayersreference[i].run();
 
@@ -48,6 +48,6 @@
 
         System.out.println("Not yet implemented");
 
-    }     
- 
+    }
+
 }
