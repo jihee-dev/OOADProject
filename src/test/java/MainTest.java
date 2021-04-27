0aec35b5ab43e13011f497eb8cd7d7a4c76ac847
union93 <union93@konkuk.ac.kr>
2021-04-28 05:00:28 +0900

    "pmd_add"

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -4,33 +4,9 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
 class MainTest {
-
     @Test
-    void getName() {
-    }
+    void main() {
 
-    @Test
-    void getAge() {
-    }
 
-    @Test
-    void getTall() {
-    }
-
-    @Test
-    void setName(String name) {
-
-    }
-
-    @Test
-    void setAge() {
-    }
-
-    @Test
-    void setTall() {
-    }
-
-    @Test
-    void printInfo() {
     }
 }
