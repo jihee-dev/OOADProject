7404a1c956c4a498c2f9e464f2670b889d045188
plat67 <good2060@naver.com>
2021-05-04 22:39:42 +0900

    test

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,4 +2,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }
+
+    public int my_add(int a, int b) {
+        return a + b;
+    }
 }
---
+++ src/test/java/MainTest.java
@@ -0,0 +1,12 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MainTest {
+    @Test
+    public void testMyAdd() {
+        Main m = new Main();
+        int result = m.my_add(10, 20);
+        assertEquals(30, result);
+    }
+}
